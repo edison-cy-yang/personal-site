998dcc3779a2dc2d3412c6b9f0e29c520ff9992c
edison-cy-yang <edison.cy.yang@gmail.com>
2020-03-03 14:07:51 -0800

    converted footer component to functional component

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class Footer extends Component {
-  render() {
-
-    if(this.props.data){
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
-    }
-
-    return (
-      <footer>
-
-     <div className="row">
-        <div className="twelve columns">
-           <ul className="social-links">
-              {networks}
-           </ul>
-        </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
-     </div>
-  </footer>
-    );
+export default function Footer(props) {
+  let networks;
+  if (props.data) {
+    networks = props.data.social.map((network) => {
+      return (
+        <li key={network.name}>
+          <a href={network.url}>
+            <i className={network.className}></i>
+          </a>
+        </li>
+      );
+    });
   }
-}
 
-export default Footer;
+  return (
+    <footer>
+      <div className="row">
+        <div className="twelve columns">
+          <ul className="social-links">{networks}</ul>
+        </div>
+        <div id="go-top">
+          <a className="smoothscroll" title="Back to Top" href="#home">
+            <i className="icon-up-open"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
